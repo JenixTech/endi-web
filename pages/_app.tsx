2e6d9e1c426e6d0bb9ff1e960f6ebd55c41ddf8d
Will Nixon <will_nixon@me.com>
2021-06-17 08:14:36 -0500

    add chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+
+const colors = {
+  brand: {
+    primary: "#ffbbc6",
+    secondary: "#fdf111",
+    accent: "#e91311",
+  },
+}
+
+const styles = {
+  global: {
+    "html, body": {
+      padding: 0,
+      margin: 0,
+      backgroundColor: colors.brand.primary
+    }
+  }
+}
+
+const theme = extendTheme({ 
+  colors,
+  styles
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 export default MyApp
