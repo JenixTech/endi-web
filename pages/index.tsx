56140acf7bb4f13bb2ef0e913df06db348f5498b
Will Nixon <will_nixon@me.com>
2021-09-15 16:00:36 +0100

    create landingpage layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { HStack, Center, Flex, Text, Heading, Box } from '@chakra-ui/react'
 import Head from 'next/head'
 
 export default function Home() {
@@ -7,15 +8,51 @@ export default function Home() {
         <title>ENdi</title>
         <meta name="description" content="ENdi: Your personal endometriosis diary" />
       </Head>
+      
+      <Flex height='100vh' flexDirection="column">
+        <Box flexGrow={2}>
+          <main>
+            <HStack spacing="0px" height="100%">
+              <Center 
+                minWidth="500px" 
+                width={1/3} 
+                height="100%" 
+                bg="brand.secondary" 
+                alignItems="center"
+              >
+                <Text>1</Text>
+              </Center>
+              <Flex 
+                width={2/3} 
+                height="100%" 
+                justifyContent="center"
+                flexDirection="column"
+                padding="3rem"
+              >
+                <Text fontWeight="bold" fontSize="2xl">A mobile app for iOS and Android</Text>
+                <Heading 
+                  as="h1" 
+                  size="4xl"
+                  marginY="1rem"
+                  color="white"
+                  textTransform="uppercase"
+                >
+                  Coming Soon!
+                </Heading>
+                <Text fontWeight="bold" fontSize="2xl">Enter your email and we'll send you a download link as soon as it's available.</Text>
+              </Flex>
+            </HStack>
+          </main>
+        </Box>
 
-      <main>
-        <p>Hi Jemma. As this page is public, it is now public knowledge that:</p>
-        <br />
-        <p><em>You are awesome.</em></p>
-      </main>
-
-      <footer>
-      </footer>
+        <footer>
+          <Center flexGrow={1} height='100%'>
+            <HStack>
+              <Text>&copy; 2021 Jenix Technologies LTD. All Rights Reserved.</Text>
+            </HStack>
+          </Center>
+        </footer>
+      </Flex>
     </div>
   )
 }
