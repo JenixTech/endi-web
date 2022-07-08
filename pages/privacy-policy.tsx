075464669faffc7b62faa2fd5fc2c5ea337a325e
Will Nixon <will_nixon@me.com>
2022-07-08 12:35:44 -0500

    minor tweak

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -11,6 +11,7 @@ export default function PrivacyPolicy() {
     </Head>
     <h1>Privacy Policy</h1>
     <p>Last updated: May 11, 2022</p>
+    <br />
     <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
     <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
     <h1>Interpretation and Definitions</h1>
